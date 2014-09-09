7ede805fcfc525274323ba9a759ee4e27d9aa12a
Ludwig Pethrus Engstrom <ludwigpet@gmail.com>
2014-09-09 19:05:00 +0200

    Basic server

---
+++ Assets/Scripts/ClientScript.cs
@@ -0,0 +1,98 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClientScript : MonoBehaviour {
+    
+    private const string typeName = "MegatronsSuperAwesomeGame"; // this is the name of the game
+    public GameObject playerPrefab;
+    public Transform playerSpawnPoints;
+    private HostData[] hostList;
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    void OnGUI() {
+        if (Network.isClient) return;
+        
+        int cWidth = 200;
+        GUILayout.BeginArea(new Rect(Screen.width / 2 - cWidth / 2, 200, 200, 100));
+        GUILayout.BeginVertical("box");
+
+        if (hostList != null)
+        {
+            foreach (HostData host in hostList) {
+                if (GUILayout.Button(host.gameName))
+                    JoinServer(host);
+            }
+        }
+        if(GUILayout.Button("Refresh"))
+            RefreshHostList();
+        GUILayout.EndVertical();
+        GUILayout.EndArea();
+    }
+
+    private void JoinServer(HostData host) {
+        Network.Connect(host);
+    }
+    private void RefreshHostList() {
+        MasterServer.RequestHostList(typeName);
+    }
+
+    void OnMasterServerEvent(MasterServerEvent msEvent) {
+        if (msEvent == MasterServerEvent.HostListReceived)
+            hostList = MasterServer.PollHostList();
+    }
+
+    // This function is called when the client successfully has connected to the server
+    void OnConnectedToServer() {
+
+        Debug.Log("Connected to server");
+        networkView.RPC("GetPlayerType", RPCMode.Server, Network.player, SystemInfo.deviceType.ToString());
+        
+    }
+
+    void OnDisconnectedFromServer(NetworkDisconnection info) {
+        Debug.Log("Disconnected from Server. Scene will restart");
+        Application.LoadLevel(Application.loadedLevel);
+    }
+
+    [RPC]
+    void SetPlayerType(string type) {
+        switch (type) { 
+            case "PLAYER":
+                InitializePlayer();
+                break;
+            case "HELPER":
+                InitializeHelper();
+                break;
+            case "ENEMY":
+                InitializeEnemy();
+                break;
+        }
+    }
+    [RPC]
+    void GetPlayerType(NetworkPlayer player, string deviceType)
+    {
+        return;
+    }
+    private void InitializeEnemy()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    private void InitializeHelper()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    private void InitializePlayer()
+    {
+        Network.Instantiate(playerPrefab, playerSpawnPoints.position, Quaternion.identity, 0);
+    }
+
+}
--- Assets/Scripts/NetworkManager.cs
+++ Assets/Scripts/NetworkManager.cs
@@ -1,41 +1,151 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+
+public enum PlayerType { 
+    PLAYER,
+    HELPER,
+    ENEMY
+};
 
 public class NetworkManager : MonoBehaviour {
 
-    bool DEBUGGING = false;
-
-	// Use this for initialization
+    private int playerCount = 0;
+    private int maxMessages = 200;
+    private Queue messages;
     private const string typeName = "MegatronsSuperAwesomeGame"; // this is the name of the game
     private const string gameName = "theBestRoom"; // this is the name of the game room within the game
+
+    private NetworkPlayer mainPlayer;
+    private List<NetworkPlayer> helpers;
+    private List<NetworkPlayer> enemies;
+
+    public Vector2 scrollPos;
+    
 	void Start () 
     {
-        if(!DEBUGGING)
-        {
-            bool useNat = !Network.HavePublicAddress();
-            Network.InitializeServer(4, 1337, useNat);
-        }
-        
+        // initialize lists
+        helpers = new List<NetworkPlayer>();
+        enemies = new List<NetworkPlayer>();
+        messages = new Queue(maxMessages);
+
+        // initialize the server
+        bool useNat = !Network.HavePublicAddress();
+        Network.InitializeServer(4, 1337, useNat);
 	}
+
+    // The OnGUI method is called every frame and re-rendered.
     void OnGUI()
     {
- 
-        GUI.Box(new Rect(10.0f, 10.0f, 100.0f, 90.0f), "Loader Menu");
-
-        if (GUI.Button(new Rect(20, 40, 80, 20), "Level 1"))
+        if (Network.isServer)
         {
-            Application.LoadLevel("scene1");
+            // Create a scroll view and show all the messages on the server.
+            scrollPos = GUILayout.BeginScrollView(scrollPos, GUILayout.Width(Screen.width), GUILayout.Height(Screen.height));
+            foreach (string m in messages)
+            {
+                GUILayout.Label(m);
+            }
+            GUILayout.EndScrollView();
         }
 
-    }
-	// Update is called once per frame
-	void Update () 
-    {
-	
-	}
 
+    }
+    
     void OnServerInitialized()
     {
-        Debug.Log("Server initialized and ready");
+        AddMessage("Server initialized registering with master server...");
+        MasterServer.RegisterHost(typeName, gameName);
     }
+
+    void OnMasterServerEvent(MasterServerEvent msEvent)
+    {
+        switch (msEvent)
+        { 
+            case MasterServerEvent.RegistrationSucceeded:
+                AddMessage("Server registered successfully");
+                break;
+
+            case MasterServerEvent.RegistrationFailedGameType:
+                AddMessage("Failed to register gametype: " + typeName);
+                break;
+
+            case MasterServerEvent.RegistrationFailedGameName:
+                AddMessage("Failed to register Game name: " + gameName);
+                break;
+        }
+   } 
+
+    void OnPlayerConnected(NetworkPlayer player)
+    {
+        AddMessage("Player " + playerCount++ + " connected from " + player.ipAddress + ":" + player.port);
+    }
+
+    void OnPlayerDisconnected(NetworkPlayer player)
+    {
+        AddMessage("Clean up after player " + player);
+     
+        Network.RemoveRPCs(player);
+        Network.DestroyPlayerObjects(player);
+        playerCount--;
+        AddMessage("Player " + player.ipAddress + " has disconnected. (players left: " + playerCount + ")" );
+
+        if (player == mainPlayer)
+            // we should probably end the game here 
+            DisconnectGame();
+        else if (helpers.Contains(player))
+            helpers.Remove(player);
+        else if (enemies.Contains(player))
+            enemies.Remove(player);
+
+    }
+
+    private void DisconnectGame()
+    {
+        Network.Disconnect();
+        playerCount = 0;
+
+        // these might not be needed if OnPlayerDisconnected is fired
+        helpers.Clear();
+        enemies.Clear();
+    }
+
+    [RPC]
+    void GetPlayerType(NetworkPlayer player, string deviceType) {
+        PlayerType type = PlayerType.PLAYER;
+        switch (deviceType) { 
+            case "Handheld":
+                type = (helpers.Count > enemies.Count)? PlayerType.ENEMY : PlayerType.HELPER;
+                if (helpers.Count > enemies.Count)
+                {
+                    type = PlayerType.ENEMY;
+                    enemies.Add(player);
+                }
+                else {
+                    type = PlayerType.HELPER;
+                    helpers.Add(player);
+                }
+                break;
+            default:
+                type = PlayerType.PLAYER;
+                mainPlayer = player;
+                break;
+        }
+        AddMessage("Player " + player.ipAddress + " with devicetype: " + deviceType + " is a " + type.ToString());
+
+        networkView.RPC("SetPlayerType", player, type.ToString());
+    }
+
+    [RPC]
+    void SetPlayerType(string type)
+    {
+        return;
+    }
+
+
+    void AddMessage(string message) {
+        if (messages.Count >= maxMessages) messages.Dequeue();
+        messages.Enqueue(message);
+    }
+
+
 }
