480c302a8e5b918f5995a96b9c46386d66fcfe62
Christoffer <christoffer.wiss@gmail.com>
2014-09-12 17:24:05 +0200

    [GamePlay] Player can now consume pellets. When all pellets have been eaten a victory text is shown.

---
+++ Assets/GameController.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+	// Keeps track of nr of pellets left, when zero => victory
+	private int nrPelletsLeft;
+
+	// Text displayed at completion of game
+	public GUIText victoryText;
+	private bool gameIsOver = false;
+
+	// Score counter
+	public GUIText scoreText;
+	private int scoreCounter;
+
+	// Use this for initialization
+	void Start () {
+		scoreCounter = 0;
+		UpdateScore ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if( !victoryText.guiText.enabled && nrPelletsLeft <= 0 ){
+			victoryText.guiText.enabled = true;
+			gameIsOver = true;
+		}
+	}
+	
+	/// <summary>
+	/// Adds points to the score.
+	/// </summary>
+	/// <param name="points">Amount of points to add.</param>
+	public void AddScore(int points){
+		scoreCounter += points;
+		UpdateScore ();
+	}
+
+	/// <summary>
+	/// Increments the pellet counter.
+	/// </summary>
+	public void IncrementPelletCounter(){
+		nrPelletsLeft++;
+	}
+
+	/// <summary>
+	/// Decrements the pellet counter.
+	/// </summary>
+	public void DecrementPelletCounter(){
+		nrPelletsLeft--;
+	}
+
+	/// <summary>
+	/// Updates the score.
+	/// </summary>
+	void UpdateScore() {
+		scoreText.text = "Score: " + scoreCounter;
+	}
+}
---
+++ Assets/Scripts/Pellet_Collision.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pellet_Collision : MonoBehaviour {
+
+	// How many points one pellet is worth
+	public int scoreValue = 10;
+
+	// Link to the game controller
+	private GameController gameController;
+
+	// Use this for initialization
+	void Start () {
+		GameObject gameControllerObject = GameObject.FindWithTag ("GameController");
+		if (gameControllerObject != null)
+		{
+			gameController = gameControllerObject.GetComponent <GameController>();
+		}
+		else
+		{
+			Debug.Log ("Cannot find 'GameController' script");
+		}
+		gameController.IncrementPelletCounter();
+	}
+	
+	/// <summary>
+	/// Something has collided with the pellet.
+	/// </summary>
+	/// <param name="other">Collider object.</param>
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.CompareTag("Player")) {
+			gameController.AddScore(scoreValue);
+			gameController.DecrementPelletCounter();
+			Destroy (this.gameObject);
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
