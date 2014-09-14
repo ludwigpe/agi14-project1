6e00f192b05f3e73b728eb550843412bec9f6bb4
Christoffer <christoffer.wiss@gmail.com>
2014-09-15 00:35:11 +0200

    Small fixes to Pacman prefab

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -6,21 +6,9 @@ public class Movement : MonoBehaviour {
 	public float jumpSpeed = 8.0F;
 	public float gravity = 20.0F;
 	private Vector3 moveDirection = Vector3.zero;
-
-    // Link with GameController
-    private GameController gameController;
 	
     // Use this for initialization
     void Start () {
-        GameObject gameControllerObject = GameObject.FindWithTag("GameController");
-        if (gameControllerObject != null)
-        {
-            gameController = gameControllerObject.GetComponent<GameController>();
-        }
-        else
-        {
-            Debug.Log("Cannot find 'GameController' script");
-        }
 	}
 	
 	// Update is called once per frame
@@ -36,11 +24,5 @@ public class Movement : MonoBehaviour {
 		}
 		moveDirection.y -= gravity * Time.deltaTime;
 		controller.Move(moveDirection * Time.deltaTime);
-
-        // Death check
-        if (transform.position.y < -100){
-            gameController.GameLost = true;
-            Destroy(this.gameObject);
-        }
 	}
 }
