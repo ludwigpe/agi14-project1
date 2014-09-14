bdeef1ad830a5033bb4912a98483abbb5e59039a
Christoffer <christoffer.wiss@gmail.com>
2014-09-15 00:08:27 +0200

    [Various] Pacman model, game over checks, restart button.

--- Assets/GameController.cs
+++ Assets/GameController.cs
@@ -3,12 +3,23 @@ using System.Collections;
 
 public class GameController : MonoBehaviour {
 
+    // Connection to player object
+    public GameObject player;
+
+    // Game time 
+    public GUIText timeText;
+    private int secondsPassed;
+    private const int MAX_TIME = 255;
+
 	// Keeps track of nr of pellets left, when zero => victory
 	private int nrPelletsLeft;
 
 	// Text displayed at completion of game
 	public GUIText victoryText;
-	private bool gameIsOver = false;
+    public GUIText failureText;
+	private bool gameLost = false;
+    private bool gameWon = false;
+    private bool gameIsOver = false;
 
 	// Score counter
 	public GUIText scoreText;
@@ -17,16 +28,62 @@ public class GameController : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		scoreCounter = 0;
+        secondsPassed = 0;
 		UpdateScore ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if( !victoryText.guiText.enabled && nrPelletsLeft <= 0 ){
-			victoryText.guiText.enabled = true;
-			gameIsOver = true;
-		}
+        if (!gameIsOver)
+        {
+            UpdateTimeText();
+            CheckVictoryConditions();
+
+            if (gameWon)
+            {
+                victoryText.guiText.enabled = true;
+                gameIsOver = true;
+            }
+            else if (gameLost)
+            {
+                failureText.guiText.enabled = true;
+                gameIsOver = true;
+            }
+        }
+        else{
+        }
 	}
+
+    void OnGUI(){
+        if (gameIsOver){
+            // Create a restart button
+            if (GUI.Button(new Rect(Screen.width / 2 - 140, Screen.height / 2 + 30, 200, 40), "Restart"))
+            {
+                Application.LoadLevel("start");
+            }
+        }
+    }
+
+    /// <summary>
+    /// Updates the time text to time passed since beginning of the game in seconds.
+    /// </summary>
+    private void UpdateTimeText(){
+        secondsPassed = (int)Mathf.Floor(Time.timeSinceLevelLoad);
+        timeText.text = "Time: " + secondsPassed;
+    }
+
+    /// <summary>
+    /// Check victory/losing conditions.
+    /// </summary>
+    private void CheckVictoryConditions(){
+        if (nrPelletsLeft <= 0){
+            gameWon = true;
+        }
+        else if (secondsPassed > MAX_TIME){
+            gameLost = true;
+            Destroy(player.gameObject);
+        }
+    }
 	
 	/// <summary>
 	/// Adds points to the score.
@@ -57,4 +114,29 @@ public class GameController : MonoBehaviour {
 	void UpdateScore() {
 		scoreText.text = "Score: " + scoreCounter;
 	}
+
+    #region Accessors
+    public bool GameLost{
+        get
+        {
+            return gameLost;
+        }
+        set
+        {
+            gameLost = value;
+        }
+    }
+
+    public bool GameWon
+    {
+        get
+        {
+            return gameWon;
+        }
+        set
+        {
+            gameWon = value;
+        }
+    }
+    #endregion
 }
