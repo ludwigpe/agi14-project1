8f70e06c0a822c1e332462f1e2579f53f6dd13ec
Christoffer <christoffer.wiss@gmail.com>
2014-09-18 22:02:41 +0200

    Created PlaySoundEffect script
    Soundeffects for Pac-Man (aka 2-Pacs) can now be played without needing
    to place AudioSources in scene.

--- Assets/Scripts/IceControls.cs
+++ Assets/Scripts/IceControls.cs
@@ -10,37 +10,24 @@ public class IceControls : MonoBehaviour
     public float breakPower = 10.0F;
     public float rotationSpeed = 100.0F;
 	private Vector3 moveDirection = Vector3.zero;
-    
-    private const float MIN_VEL_MAG = 0.1f;
 
-    // AudioSources
-    public AudioSource audio_source_break;
-    public AudioSource audio_source_move;
-    private bool breakSoundStarted = false;
+    // Link to components
+    private PlaySoundEffect soundEffectManager;
+    private CharacterController charController;
 
-    // Sounds
-    public AudioClip sound_jump;
-	
     // Use this for initialization
 	void Start () 
     {
+        soundEffectManager = GetComponent<PlaySoundEffect>();
+        charController = GetComponent<CharacterController>();
 	}
 	
 	// Update is called once per frame
 	void Update () 
     {
-	    CharacterController controller = GetComponent<CharacterController>();
-
-        // Stop break sound when button is released
-        if (breakSoundStarted && !Input.GetKey(KeyCode.DownArrow))
-        {
-            audio_source_break.Stop();
-            breakSoundStarted = false;
-        }
-    
         // Rotate player around y-axis
         transform.Rotate(0, Input.GetAxis("Horizontal") * rotationSpeed * Time.deltaTime, 0);
-		if (controller.isGrounded) 
+        if (charController.isGrounded) 
         {
             moveDirection.y = 0;
             if (Input.GetKey(KeyCode.UpArrow)) 
@@ -48,28 +35,18 @@ public class IceControls : MonoBehaviour
                 // player pressed up-key so applie some force to the movement
                 Vector3 force = transform.forward * speed * Time.deltaTime;
                 moveDirection += force;
-                
-                if (!audio_source_move.isPlaying)
-                {
-                    audio_source_move.Play();
-                }
+                soundEffectManager.playMoveSound();
             }
             if (Input.GetKey(KeyCode.DownArrow)) 
             {
                 Vector3 breakForce = moveDirection * -1 * breakPower * Time.deltaTime;
                 moveDirection += breakForce;
-
-                // Make sure to only play break sound once
-                if (!audio_source_break.isPlaying && !breakSoundStarted && controller.velocity.magnitude > MIN_VEL_MAG)
-                {
-                    audio_source_break.Play();
-                    breakSoundStarted = true;
-                }
+                soundEffectManager.playBrakeSound(charController.velocity.magnitude);
             }
             if (Input.GetButton("Jump"))
             {
-                AudioSource.PlayClipAtPoint(sound_jump, transform.position);
                 moveDirection.y = jumpSpeed;
+                soundEffectManager.playJumpSound();
             }
             
             float fric = Mathf.Clamp(100 - friction, 0, 100);
@@ -77,6 +54,6 @@ public class IceControls : MonoBehaviour
             moveDirection *= fric;
 		}
 		moveDirection.y -= gravity * Time.deltaTime;
-		controller.Move(moveDirection * Time.deltaTime);
+        charController.Move(moveDirection * Time.deltaTime);
 	}
 }
