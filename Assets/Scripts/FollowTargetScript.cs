f1653c828773ba7beb1b435531cea11c04d1644c
Mattias Lönnerberg <mattias.lonnerberg@gmail.com>
2014-09-19 20:28:16 +0200

    Added A Ghost with Simple AI
    Added a ghost that updates it's postion every 10 seconds. When the ghost
    gets close it's destination updates every frame instead.

---
+++ Assets/Scripts/FollowTargetScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Component that makes an object follow another object.
+public class FollowTargetScript : MonoBehaviour {
+
+	public Transform target;
+	public float persistentChaseDistance;		// At which distance the ghost will follow more precisely
+	public float destinationUpdateFrequency;	// Amount of seconds between every update of the destination
+	NavMeshAgent agent;
+
+	void Start () {
+		agent = GetComponent<NavMeshAgent>();
+
+		// Update target position at a slowed down frequency to simulate AI stupidity
+		InvokeRepeating("UpdateDestination", 0.0f, destinationUpdateFrequency);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		// Update the target destination every frame when the two objects are close to eachother
+		if (Vector3.Distance(transform.position, target.position) <= persistentChaseDistance)
+			UpdateDestination();
+	}
+
+	// Update this objects target destination.
+	void UpdateDestination () {
+		agent.SetDestination(target.position);
+	}
+}
