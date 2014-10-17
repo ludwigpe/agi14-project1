c6e14ac411cc6fa1030f4ba716063cd67239c64b
Christoffer <christoffer.wiss@gmail.com>
2014-10-17 19:33:13 +0200

    [Misc] Cleanup

---
+++ Assets/Scripts/EMPExplosion.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EMPExplosion : MonoBehaviour {
+
+    // Update is called once per frame
+	void Update () {
+        transform.Rotate(Time.smoothDeltaTime * 200, 0, 0);
+        transform.localScale += Mathf.Sin(Time.time) * new Vector3(50* Time.smoothDeltaTime, 50*Time.smoothDeltaTime, 50*Time.smoothDeltaTime);
+	}
+
+}
