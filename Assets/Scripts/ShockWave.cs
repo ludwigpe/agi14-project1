f43837b69c005f4cb5691ccb5571454b880d7e08
Ludwig Pethrus Engstrom <ludwigpet@gmail.com>
2014-10-19 12:01:48 +0200

    ShockWave shader added
    Now we can create a shockwave animation with a shader. This shader will
    replace the self illuminated shader later for the floor because I have
    included self illumination and texture mapping into this shader. The
    animation is controlled through a script called ShockWave that will
    update the radius of the shock wave and trigger it on a key press. Right
    now that key is F4. This should later be used in conjuction with the EMP
    effect.

---
+++ Assets/Scripts/ShockWave.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShockWave : MonoBehaviour {
+
+    float radius = 0.0F;
+    public float maxRadius = 100.0F;
+    public float animationSpeed = 10.0F;
+    bool updateRadius = false;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.F4))
+        { 
+            updateRadius = true;
+            radius = 0.0F;
+        }
+        if(updateRadius)
+        {
+            radius += Time.deltaTime * animationSpeed;
+            renderer.material.SetFloat("_Radius", radius);
+            if (radius > maxRadius)
+                updateRadius = false;
+        }
+	}
+}
