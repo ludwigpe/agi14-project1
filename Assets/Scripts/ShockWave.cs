bd0339e81ed4718e85229491d6e30a55c98e2c06
Ludwig Pethrus Engstrom <ludwigpet@gmail.com>
2014-10-23 15:24:01 +0200

    Shader changed to surface shader

--- Assets/Scripts/ShockWave.cs
+++ Assets/Scripts/ShockWave.cs
@@ -1,59 +1,54 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+/// <summary>
+/// This script is responsible for starting and stopping the shockwave.
+/// It updates the radius of the shockwave so the shader knows how far the blast
+/// has gone. It will be triggered by pacman
+/// </summary>
 public class ShockWave : MonoBehaviour {
 
     float radius = 0.0F;
     public float maxRadius = 100.0F;
     public float animationSpeed = 10.0F;
-    public float glow = 5.0F;
-    public Shader regular;
-    public Shader shockwave;
     bool updateRadius = false;
 	// Use this for initialization
 	void Start () {
-        renderer.material.shader = regular;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetKeyDown(KeyCode.F4))
-        { 
-          // Vector3 origin = GameObject.Find("Pacman").transform.position;
-           Vector3 origin = Vector3.one;
-           StartShockWave(origin);
-        }
         if(updateRadius)
         {
             radius += Time.deltaTime * animationSpeed;
-            Debug.Log(radius);
-            //glow = Mathf.Lerp(glow, 1.0F, Time.deltaTime);
-           // glow = glow / radius;
             renderer.material.SetFloat("_Radius", radius);
-            renderer.material.SetFloat("_SelfIllu",  Mathf.Lerp(glow, 1.0F, Time.deltaTime));
 
             if (radius > maxRadius)
             {
                 StopShockWave();
-
             }
-                
         }
 	}
 
-    void StartShockWave(Vector3 origin)
+    /// <summary>
+    /// Starts the shockwave. It sets the shaders origin to let it know where 
+    /// the shockwave originates from and initializes the radius to 15 so 
+    /// pacman is not below the shockwave in the beginning.
+    /// </summary>
+    /// <param name="origin"></param>
+    public void StartShockWave(Vector3 origin)
     {
-        renderer.material.shader = shockwave;
         Vector4 o = new Vector4(origin.x, origin.y, origin.z);
         o.w = 1.0F;
         renderer.material.SetVector("_Origin", o);
         updateRadius = true;
-        radius = 0.0F;
+        radius = 15.0F;
     }
-
+    /// <summary>
+    /// Stop updating the shockwave radius.
+    /// </summary>
     void StopShockWave()
     {
-        renderer.material.shader = regular;
         updateRadius = false;
     }
 }
